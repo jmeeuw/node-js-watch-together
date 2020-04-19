08d3178876edf7737f99bc55c98b842afae81d6a
Joost M <meeuwisse04@gmail.com>
2020-04-19 19:25:54 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const socket = require("socket.io");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("public"));
+
+//Run the server
+const server = app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
+
+// setup sockets
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("Connected");
+  socket.on("update", (data) => {
+    console.log(data);
+    socket.broadcast.emit("update", data);
+  });
+  socket.on("play", () => {
+    socket.broadcast.emit("play");
+  });
+  socket.on("pause", () => {
+    socket.broadcast.emit("pause");
+  });
+  socket.on("slider", (data) => {
+    socket.broadcast.emit("slider", data);
+  });
+});

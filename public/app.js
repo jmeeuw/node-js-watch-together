d24ceea10c450945daa206d261ddb01ff336da65
Joost M <meeuwisse04@gmail.com>
2020-04-19 21:13:42 +0200

    heroku

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-let socket = io.connect("http://localhost:3000");
+let socket = io.connect("https://watch-together-now.herokuapp.com");
 
 // Extracted buttons
 let buttons = document.getElementsByTagName("button");
